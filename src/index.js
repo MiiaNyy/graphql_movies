d84ea096f1358cea70cb771ede4a11cd90a3fd48
MiiaNyy <mi.nyyssonen@gmail.com>
2022-07-29 11:44:38 +0300

    connected app to mongodb database

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+import 'dotenv/config'
+import mongoose from 'mongoose';
 import { ApolloServer } from "apollo-server";
+
 import { typeDefs } from "./typeDefs";
 import { resolvers } from "./resolvers";
+
+const uri = process.env.MONGODB_URI
+const main = async () => {
+    await mongoose.connect( uri, { useNewUrlParser: true, useUnifiedTopology: true } );
+};
+
+main()
+    .then( () => console.log( '🎉 Connected to database successfully' ) )
+    .catch( error => console.error( error ) );
 
 const server = new ApolloServer( {
     typeDefs,
     resolvers
 } );
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
-});
+server.listen( { port: process.env.PORT || 4000 } ).then( ({ url }) => {
+    console.log( `🚀 Server ready at ${ url }` );
+} );
